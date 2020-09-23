ae984e9d44ea7873e26755a214ae43e8e31414bb
Riste Drangovski <riste.drangovski@codeit.mk>
2020-09-23 17:29:40 +0200

    en lang hardcoded

--- src/HEREMap.tsx
+++ src/HEREMap.tsx
@@ -75,7 +75,7 @@ export const HEREMap: React.FC<HEREMapProps> = ({
     if (platform) {
       const defaultLayers = platform.createDefaultLayers({
         ppi: hidpi ? 320 : 72,
-        lg: "zh"
+        lg: "en",
       });
 
       const mapElement = document.querySelector('#map-container');
